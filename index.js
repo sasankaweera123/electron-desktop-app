c252c8c6c664d6a8e514427fb63521821c8cd5c7
SasankaWeera <sasanka.weerakoon@aiken.com.lk>
2024-05-15 14:31:43 +0530

    Add index boiler plate
    - update content from html

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const electron = require('electron');
 
-const {app} = electron;
+const {app, BrowserWindow} = electron;
 
 app.on('ready', () => {
-    console.log('The application is ready.');
+    const mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
