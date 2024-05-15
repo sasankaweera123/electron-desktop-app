1f2bf9320479e210fc4ba78d7312b274df1fc553
SasankaWeera <sasanka.weerakoon@aiken.com.lk>
2024-05-15 16:58:03 +0530

    Update based on preload file

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const electron = require('electron');
 const ffmpeg = require('fluent-ffmpeg');
+const path = require('path');
 
 const {app, BrowserWindow, ipcMain} = electron;
 
 let mainWindow;
 
+const isDev = process.env.NODE_ENV === 'development';
+
 app.on('ready', () => {
     mainWindow = new BrowserWindow({
         webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false
+            nodeIntegration: false,
+            contextIsolation: true,
+            preload: path.join(__dirname, 'preload.js')
         }
     });
-    mainWindow.loadURL(`file://${__dirname}/index.html`);
+    mainWindow.loadURL(`file://${__dirname}/index.html`).then(() => {
+        if (isDev) {
+            mainWindow.webContents.openDevTools();
+        }
+    });
 });
 
 ipcMain.on('video:submit', (event, path) => {
