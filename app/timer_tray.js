de1122cba934ec89f7aeaefbe9627032f8daa192
SasankaWeera <sasanka.weerakoon@aiken.com.lk>
2024-05-16 16:16:45 +0530

    Create timer_tray.js

---
+++ app/timer_tray.js
@@ -0,0 +1,43 @@
+const electron = require('electron');
+const { app,Tray, Menu } = electron;
+
+class TimerTray extends Tray {
+    constructor(iconPath, mainWindow) {
+        super(iconPath);
+
+        this.mainWindow = mainWindow;
+
+        this.on('click', this.onClick.bind(this));
+        this.on('right-click', this.onRightClick.bind(this));
+    }
+
+    onClick(event, bounds) {
+        const { x, y } = bounds;
+        const { height, width } = this.mainWindow.getBounds();
+        if (this.mainWindow.isVisible()) {
+            this.mainWindow.hide();
+        } else {
+            const yPosition = process.platform === 'darwin' ? y : y - height;
+            this.mainWindow.setBounds({
+                x: x - width / 2,
+                y: yPosition,
+                height,
+                width,
+            });
+            this.mainWindow.show();
+        }
+    }
+
+    onRightClick() {
+        const contextMenu = Menu.buildFromTemplate([
+            {
+                label: 'Quit',
+                click: () => app.quit(),
+            },
+        ]);
+        this.popUpContextMenu(contextMenu);
+    }
+
+}
+
+module.exports = TimerTray;
