eb2be3a71ab54a1287f1a2d3247da07da170ff5b
SasankaWeera <sasanka.weerakoon@aiken.com.lk>
2024-05-16 11:35:44 +0530

    Create Todo application boiler plate

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const electron = require('electron');
-const ffmpeg = require('fluent-ffmpeg');
 const path = require('path');
 
-const {app, BrowserWindow, ipcMain} = electron;
+const {app, BrowserWindow, Menu} = electron;
 
 let mainWindow;
+let addWindow;
 
 const isDev = process.env.NODE_ENV === 'development';
 
+/**
+ * Create the main window when the application is ready.
+ */
 app.on('ready', () => {
     mainWindow = new BrowserWindow({
         webPreferences: {
-            nodeIntegration: false,
-            contextIsolation: true,
+            nodeIntegration: true,
+            contextIsolation: false,
             preload: path.join(__dirname, 'preload.js')
         }
     });
@@ -21,10 +24,52 @@ app.on('ready', () => {
             mainWindow.webContents.openDevTools();
         }
     });
+
+    // Quit the application when the main window is closed.
+    mainWindow.on('closed', () => app.quit());
+
+    // Create the application menu.
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
 });
 
-ipcMain.on('video:submit', (event, path) => {
-    ffmpeg.ffprobe(path, (err, metadata) => {
-        mainWindow.webContents.send('video:metadata', metadata.format);
+function createAddWindow() {
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add New Todo'
     });
-});
+    addWindow.loadURL(`file://${__dirname}/add.html`);
+}
+
+/**
+ * Menu template for the application.
+ * @type {[{submenu: [{label: string},{accelerator: (string), label: string, click(): void}], label: string}]}
+ */
+const menuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Add New Todo',
+                click() {
+                    createAddWindow();
+                }
+            },
+            {
+                label: 'Quit',
+                accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                click() {
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
+
+/**
+ * Add an empty object to the beginning of the menuTemplate array if the platform is macOS.
+ */
+if (process.platform === 'darwin') {
+    menuTemplate.unshift({});
+}
